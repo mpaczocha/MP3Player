0dc5258fe14ddb8001338bfc67f00d9cba54ef45
m_paczocha <m_paczocha@o2.pl>
2017-02-02 22:57:02 +0100

    Commit1

---
+++ src/pl/javastart/mp3player/main/Main.java
@@ -0,0 +1,28 @@
+package pl.javastart.mp3player.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        final String appName = "Mp3Player v0.3";
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root,400,400);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setTitle(appName);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
