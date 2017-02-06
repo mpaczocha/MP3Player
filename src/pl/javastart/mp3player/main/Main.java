653701fb898e61dea46760939906c191879cf134
m_paczocha <m_paczocha@o2.pl>
2017-02-06 19:02:55 +0100

    Commit2

--- src/pl/javastart/mp3player/main/Main.java
+++ src/pl/javastart/mp3player/main/Main.java
@@ -1,23 +1,23 @@
 package pl.javastart.mp3player.main;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-        final String appName = "Mp3Player v0.3";
+        final String appName = "Mp3Player v0.4";
         try {
-            BorderPane root = new BorderPane();
-            Scene scene = new Scene(root,400,400);
-            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            Parent parent = (Parent) FXMLLoader.load(getClass().getResource(
+                    "/pl/javastart/mp3player/view/MainPane.fxml"));
+            Scene scene = new Scene(parent);
             primaryStage.setTitle(appName);
             primaryStage.setScene(scene);
             primaryStage.show();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
